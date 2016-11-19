f34c751aeb8c86170e06089dc06482454658b787
Sergey Makarenko <sergey.king96@gmail.com>
2016-11-19 20:33:22 +0300

    Небольшое изменение каталогов, создание resource.html с popup формой, также сопровождающие ее css и js файлы.

---
+++ src/main/webapp/WEB-INF/resource/js/popup.js
@@ -0,0 +1,9 @@
+/**
+ * Created by We on 19.11.2016.
+ */
+function popup_show() {
+    document.getElementById("popup_div").style.display = "block";
+}
+function popup_hide() {
+    document.getElementById("popup_div").style.display = "none";
+}
